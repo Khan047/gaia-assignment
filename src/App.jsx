5a773cf728ed9a7f60a45296d95498db38a2df3c
Tajammul Khan <tajzz.tk@gmail.com>
2023-12-01 19:52:52 +0530

    setup router

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,21 @@
 // Import necessary dependencies
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'; // Updated import
+
 import RegistrationPage from './pages/RegistrationPages';
 import LoginPage from './pages/LoginPages';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        {/* Route for the Registration Page */}
-        <Route exact path="/register">
-          <RegistrationPage />
-        </Route>
+    <BrowserRouter>
+      <Routes> 
+        <Route path="/register" element={<RegistrationPage />} />
 
-        {/* Route for the Login Page */}
-        <Route exact path="/login">
-          <LoginPage />
-        </Route>
+        <Route path="/login" element={<LoginPage />} />
 
-        {/* Default Route or 404 Page */}
-        {/* Add a default route or 404 page if needed */}
-      </Switch>
-    </Router>
+        <Route path="*" element={<Navigate to="/register" />} /> {/* Define a default route */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
